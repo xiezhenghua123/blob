978dcf25ab718c08aa83ecc02a0b62e75eef7c36
谢正华“ <1803493121@qq.com>
2021-06-26 15:16:32 +0800

    修改主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,25 @@
 module.exports = {
-  title: "菜鸡小谢",
-  description: "学习之余瞎写点东西",
-  head: [["link", { rel: "icon", href: "/logo.jpg" }]],
-  themeConfig: {
-    logo: "/logo.jpg",
-    lastUpdated: "Last Updated",
-    nav: [
-      {
-        text: "首页",
-        link: "/",
-      },
-      {
-        text: "前端基础相关",
-        ariaLabel: "Language Menu",
-        items: [{ text: "数组api详解", link: "array/" }],
-      },
+    title: "前端坎坷之路", //这里是博客标题
+    description: '日常学习总结，没事儿多来逛逛', //博客描述
+    dest: 'public', //博客部署时输出的文件夹
+    head: [
+        ['link', {}], //favicon图标设置
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
     ],
-  },
-};
+    theme: 'hope', //vuepress挂载的主题
+    themeConfig: {
+        darkmode: 'auto-switch',
+        hostname: ' https://mister-hope.github.io',
+        blog: {
+            avatar: '/logo.jpg',
+            name: '菜鸡小谢',
+            links: {
+                'QQ': 1803493121,
+                'Wechat': 'XZH_ddl'
+            }
+        }
+    },
+    markdown: {
+        lineNumbers: true
+    }
+}
--- docs/readme.md
+++ docs/readme.md
@@ -1,11 +1,7 @@
 ---
-home: true
-heroText: 没事儿来逛逛
-heroImage: /logo.jpg
-features:
-  - title: 前端学习
-    details: 前端学习的一些内容。
-  - title: 错误处理
-    details: 平常项目中一些常见的错误。
-footer: Copyright © 2021-present zhenghua_xie
+blog: true
+bgImage: "/home.jpg"
+bgImageStyle:
+  { "background-size": "100% 100%", "background-repeat": "no-repeat" }
+heroFullScreen: true
 ---
